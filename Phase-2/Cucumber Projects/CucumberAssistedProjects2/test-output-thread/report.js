c463b02514d7970494b7e89855deaf446aee54bd
chandu <chandusai2000@gmail.com>
2024-01-30 12:11:06 +0530

    jenkinssample

---
+++ Phase-2/Cucumber Projects/CucumberAssistedProjects2/test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"2a8cc62b-de2a-4261-a95d-709832dd1184","feature":"Register multiple users on the Rediff account Page","scenario":"Test rediff Register Account Page","start":1706596607548,"group":1,"content":"","tags":"","end":1706596627517,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
